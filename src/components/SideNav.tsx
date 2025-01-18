32cc30caa3173594e3f989cfb6404f1a83fa6dcc
Artium Wasse <artium.wasse@vimcar.com>
2025-01-18 18:19:47 +0100

    feat(SideNav): Update timer display format in SideNav component
    - Integrated formatted time display in the SideNav component using formatTimerRunning function.
    - Enhanced user experience by providing a more readable timer format instead of raw time values.

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,5 +1,6 @@
 import { useHashChange } from '../hooks/useHashchange';
 import { Time } from '../App';
+import { formatTimerRunning } from './Timer/utils';
 
 type Props = {
   times: Time[];
@@ -11,9 +12,11 @@ const Item = ({ time }: { time: Time }) => {
   // [TODO]: why this makes delete animation not work?
   const isActive = id === time.id;
 
+  const formattedTime = formatTimerRunning(time.time, 0);
+
   return (
     <a href={`#${time.id}`} key={time.id} className="group flex items-center justify-end gap-4">
-      <div className="text-sm text-white">{time.time}</div>
+      <div className="text-sm text-white">{formattedTime}</div>
       <div
         data-active={isActive}
         className="mt-2 h-1 w-5 origin-right rounded-full bg-white transition-transform duration-75 ease-in group-hover:scale-x-125 data-[active=true]:scale-x-150 data-[active=true]:group-hover:scale-x-150"
